33e6601a3bb6b74ed83eb0a09a654cc46477641e
khonsryvorleak <khonsreyvorleak5796@gmail.com>
2025-05-25 23:29:58 +0700

    commit header

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SkillsWithQuestionsAndRatings from './pages/survey/survey.js';
 import SurveyResultPage from './pages/Result/result.js';
+import Home from './pages/Home/home.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<SkillsWithQuestionsAndRatings />} />
+      <Route path="/skillwithquestionandratings" element={<SkillsWithQuestionsAndRatings />} />
       <Route path="/student-results/:studentId" element={<SurveyResultPage />} />
+      <Route path="/" element={<Home />} />
+
 
     </Routes>
   </BrowserRouter>
 );
+
+
